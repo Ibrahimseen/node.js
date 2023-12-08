aa2ab3add007e09703cf56a903a185980bd383b5
Ibrahim <ibrahimrehmanibrahim@gmail.com>
2023-12-08 20:08:30 +0500

    initial commit

---
+++ feature.js
@@ -0,0 +1,11 @@
+// const gfname = "Programming";
+// export const gfname1 = "Programming1";
+// export const gfname2 = "Programming2";
+
+// export default gfname;
+// // export {gfname1,gfname2}
+
+
+export const generateloverpercent = () => {
+return `${Math.floor(Math.random()*100)}%`;
+}
